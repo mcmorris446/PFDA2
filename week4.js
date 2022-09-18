a397a26ac698754fdf705eb2dc8b66dcc8c05954
mcmorris446 <100178569+mcmorris446@users.noreply.github.com>
2022-09-18 10:02:21 +1000

    added notes and week5

--- week4.js
+++ week4.js
@@ -9,30 +9,32 @@ function setup() {
 
 function draw() {
   background(255);
+  // set target in center (text will be slightly off enter because of this)
    let midx = 250;
  let midy = 250;
   
  
   
-// Example showing RADIUS and CENTER ellipsemode with 2 overlaying ellipses
+// to make taget symbol
 
   ellipseMode(RADIUS);
 fill(247,18,18);
-ellipse(midx, midy, 50, 50); // Outer white ellipse
+ellipse(midx, midy, 50, 50); // Outer red ellipse
 ellipseMode(CENTER);
 fill(255);
-ellipse(midx, midy, 60, 60 ); // Inner gray ellipse
+ellipse(midx, midy, 60, 60 ); // Inner whiteellipse
   
   ellipseMode(CENTER);
 fill(247,18,18);
-ellipse(midx, midy, 30, 30);; // Inner gray ellipse
+ellipse(midx, midy, 30, 30);; // center red ellipse
   
+// to make target text
 textSize(40);
   fill(0,0,0)
   
 text('Target', midx -55, midy +80)
 
-  line(midx-55, midy+85, midx+55, midy+85);
+  line(midx-55, midy+85, midx+55, midy+85); // to underline
   
   
 }
--- week5.js
+++ week5.js
@@ -2,7 +2,7 @@
     Paste the code for your week 5 exercise below.
 */
 let rad = 60; // Width of the shape
-let xpos, ypos; // Starting position of shape
+let xpos, ypos; // Starting position of shape to be later determined
 
 let xspeed = 2.8; // Speed of the shape
 let yspeed = 4.2; // Speed of the shape
@@ -10,19 +10,23 @@ let yspeed = 4.2; // Speed of the shape
 let xdirection = 1; // Left or Right
 let ydirection = 1; // Top to Bottom
 
+
 function setup() {
+  stroke(25);
   if( windowWidth > windowHeight ){
     createCanvas(windowHeight, windowHeight);
   }else{
     createCanvas(windowWidth, windowWidth);
+     
   }
 
-  hw = width/2;
+  hw = width/2; // Note to self; Hw half width -cant remember 
+  //who i saw use this but very logicall
   hh = height/2;
-  noStroke();
+
   frameRate(30);
   ellipseMode(RADIUS);
-  // Set the starting position of the shape
+  // Set the starting position of the shape (middle of screen)
   xpos = width / 2;
   ypos = height / 2;
 }
@@ -31,12 +35,12 @@ function draw() {
   background(255);
   
 
-  // Update the position of the shape
+  // Updates the position of the shape
   xpos = xpos + xspeed * xdirection;
   ypos = ypos + yspeed * ydirection;
 
-  // Test to see if the shape exceeds the boundaries of the screen
-  // If it does, reverse its direction by multiplying by -1
+  // use to set boundaries
+  //if positive temparily speeds up process until icon shoots off
   if (xpos > width - rad || xpos < rad) {
     xdirection *= -1;
   }
@@ -44,11 +48,11 @@ function draw() {
     ydirection *= -1;
   }
 
-  // Draw the shape
+  // target circles plus text
   
   ellipseMode(RADIUS);
   ellipse(xpos, ypos, rad, rad);
-fill(255);
+  fill(255); // only effects circle identified with mode
   ellipseMode(RADIUS);
   ellipse(xpos, ypos, rad-25, rad-25);
    fill(247,18,18);
@@ -57,7 +61,23 @@ fill(255);
    fill(247,18,18);
   
 textSize(40);
-  text('Target', xpos-55, ypos+90,);
+  text('Target', xpos-55, ypos+90); //offset from center - place under symbols
+  
+  //crosshairs movement
+   line(0, hh, width, hh);
+  hh = hh - 5;
+  line(hw, 0, hw, height);
+  hw = hw - 4;
+  
+  if (hh < 0) { // return to center when edges reached
+    hh = height;
+    
+  }
+  if (hw < 0) { // return to center when edges reached
+    hw = width;
+    
+  }
+ 
   
 }
   
